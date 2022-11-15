3b00a2187980d6e95bc370fa643672dc91c22821
IzumiShaka-desu <shakaaji29@gmail.com>
2022-11-15 09:01:14 +0700

    add search route

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -15,6 +15,7 @@ func Routes() {
 	route.GET("/product", controllers.GetAllProduct)
 	route.GET("/product/:id_product", controllers.GetProduct)
 	route.PATCH("/product/:id_product", controllers.UpdateProduct)
+	route.GET("/product/search", controllers.SearchProduct)
 	// route.DELETE("/todo/:idTodo", controllers.DeleteTodo)
 	hostPorts := os.Getenv("HOST_PORT")
 	hostName := os.Getenv("HOST_NAME")
