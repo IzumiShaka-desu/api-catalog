077978203dbd63a03f8487a4ffedba79758eca00
IzumiShaka-desu <shakaaji29@gmail.com>
2022-11-18 14:10:00 +0700

    create router for getting product filters

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -13,8 +13,10 @@ func Routes() {
 	route.Static("/image", "./image")
 	// route.POST("/todo", controllers.CreateTodo)
 	route.GET("/product", controllers.GetAllProduct)
+	route.GET("/product/filters", controllers.GetProductFilters)
 	route.GET("/product/:id_product", controllers.GetProduct)
 	route.GET("/product/:id_product/gallery", controllers.GetGalleryProduct)
+
 	route.GET("/product/categories", controllers.GetAllBatteryType)
 	route.PATCH("/product/:id_product", controllers.UpdateProduct)
 	route.GET("/product/search", controllers.SearchProduct)
