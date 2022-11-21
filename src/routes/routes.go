af20daf3b6e38663162a0a2ba74c00d96ca90dd8
IzumiShaka-desu <shakaaji29@gmail.com>
2022-11-21 13:42:22 +0700

    register wishlist route

--- src/routes/routes.go
+++ src/routes/routes.go
@@ -16,10 +16,12 @@ func Routes() {
 	route.GET("/product/filters", controllers.GetProductFilters)
 	route.GET("/product/:id_product", controllers.GetProduct)
 	route.GET("/product/:id_product/gallery", controllers.GetGalleryProduct)
+	route.POST("/product/:id_product/wishlist", controllers.PostFavouriteProduct)
 
 	route.GET("/product/categories", controllers.GetAllBatteryType)
 	route.PATCH("/product/:id_product", controllers.UpdateProduct)
 	route.GET("/product/search", controllers.SearchProduct)
+
 	// route.DELETE("/todo/:idTodo", controllers.DeleteTodo)
 	hostPorts := os.Getenv("HOST_PORT")
 	hostName := os.Getenv("HOST_NAME")
